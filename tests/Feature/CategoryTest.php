ebf2e661d7e10036a65b029f74ec9c660d17aa13
larry <larryjmpk@gmail.com>
2020-01-23 22:09:03 -0800

    ran category tests

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Category;
 
 class CategoryTest extends TestCase
 {
@@ -18,5 +19,33 @@ class CategoryTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    public function testGetAllCategories(){
+        \factory(Category::class,5)->create();
+        $response = $this->json('GET','/api/categories');
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            [
+                'id',
+                'name',
+                'description',
+            ]
+        ]);
+    }
+
+    public function testCreateCategory(){
+        //$user = factory(User::class)->create();
+        $category = factory(Category::class)->make()->toArray();
+        $response = $this->json('POST','/api/categories',$category);
+        $response->assertStatus(201);
+    }
+
+    public function testUpdateCategory(){
+        //$user = factory(User::class)->create();
+        $category = factory(Category::class)->create();
+        $data = ['name'=>'new category', 'description'=>'halla'];
+        $response = $this->json('PUT',"/api/categories/{$category->id}",$data);
+        $response->assertStatus(201);
     }
 }
