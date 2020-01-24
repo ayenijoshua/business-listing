ed6ff5e0034a3c9c91591f37c96935b4ddeb43ec
larry <larryjmpk@gmail.com>
2020-01-23 18:22:37 -0800

    editrd api routefile

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 //listing resource
 Route::resource('/listings', 'API\ListingController')->except('create','edit');
